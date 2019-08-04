e860d62a6adf6fdab6a8a49370dd277bccc67aa3
Greg <root@nyt.sucks>
2019-08-04 23:22:06 +0000

    ok

--- sh/marginal_annotate_racism.sh
+++ sh/marginal_annotate_racism.sh
@@ -1,8 +1,9 @@
-VERSION=2
+VERSION=3
 
 RAW_FILE=labels/racism_study${VERSION}.json
 LABEL_FILE=labels/racism_study${VERSION}.annotated.json
 touch $LABEL_FILE
 # python -m pipeline.make_json_datasets
+python -m jsql.ensure_json ${LABEL_FILE}
 python -m studies.compute_keyword_study ${RAW_FILE} racism trump
 python -m tools.label_json ${RAW_FILE} ${LABEL_FILE} gcop_v1
