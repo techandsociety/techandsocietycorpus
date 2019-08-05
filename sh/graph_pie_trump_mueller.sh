0d0a424d7a468feeeb841aacf893353dc4497b1f
Greg <root@nyt.sucks>
2019-08-05 01:39:03 +0000

    ok

--- sh/graph_pie_trump_mueller.sh
+++ sh/graph_pie_trump_mueller.sh
@@ -4,6 +4,6 @@ cat ${LABEL_FILE} | \
 python -m jsql/filter query eq "donald trump" | \
 # Note: we re-unique the queries, in case there are stories that had duplicate urls.
 python -m jsql/select_unique publication+title | \
-# Note: 'n' means 'not actually about racism'
+# Note: 'n' means 'not actually about topic'
 python -m jsql/filter ratings.gcop_v1 neq n | \
 python -m graph/pie ratings.gcop_v1 legends/trump_mueller
