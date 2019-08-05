fab5817bae6f8a4dba19228a9dfb41c930b0fb9a
Greg <root@nyt.sucks>
2019-08-05 01:34:32 +0000

    ok

--- sh/marginal_annotate_mueller.sh
+++ sh/marginal_annotate_mueller.sh
@@ -1,3 +1,8 @@
-# python -m pipeline.make_json_datasets
-python -m studies.compute_keyword_study labels/mueller_study.json mueller trump
-python -m tools.label_json labels/mueller_study.json labels/mueller_study.annotated.json q0
+VERSION=1
+
+RAW_FILE=labels/mueller_study${VERSION}.json
+LABEL_FILE=labels/mueller_study${VERSION}.annotated.json
+touch $LABEL_FILE
+python -m jsql.ensure_json ${LABEL_FILE}
+python -m studies.compute_keyword_study ${RAW_FILE} mueller trump
+python -m tools.label_json ${RAW_FILE} ${LABEL_FILE} gcop_v1 legends/trump_mueller
