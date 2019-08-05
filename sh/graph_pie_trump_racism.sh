3fabed0878c48cfb2c0495dd1677f1979f2d5eb6
Greg <root@nyt.sucks>
2019-08-05 00:27:17 +0000

    ok

--- sh/graph_pie_trump_racism.sh
+++ sh/graph_pie_trump_racism.sh
@@ -1,4 +1,8 @@
-cat labels/racism_study.annotated.json | \
+VERSION=3
+
+LABEL_FILE=labels/racism_study${VERSION}.annotated.json
+
+cat ${LABEL_FILE} | \
 python -m jsql/filter query eq "donald trump" | \
 # Note: 'n' means 'not actually about racism'
 python -m jsql/filter ratings.gcop_v1 neq n | \
