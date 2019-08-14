489b66a2eabd105cfd097208945e3a82de451f06
Greg <root@nyt.sucks>
2019-08-14 23:48:09 +0000

    only adding unique elements to db now

--- db/ingest_db.py
+++ db/ingest_db.py
@@ -14,6 +14,10 @@ import random
 import settings
 import sys
 
+query_terms = [
+	'donald trump',
+]
+
 Deref = False
 Order = FileOrders.OLDEST
 DontRegenerate = True
@@ -28,37 +32,43 @@ class Recommendation(Base):
 	query = Column(sqlalchemy.types.String(256))
 	scrape_idx = Column(Integer)
 	title = Column(sqlalchemy.types.String(1024))
+for query_term in query_terms:
+	query_parts = query_term.split(' ')
+	escaped_query_term = '%20'.join(query_parts)
+	matches = GetFilesMatching(
+		['type=news.query=' + escaped_query_term + '.', ':00:'],
+		settings.raw_data_path(),
+		Order
+		)
+	total_lines = 0
+	url = 'postgresql://tech:tech@127.0.0.1/techwatch'
+	engine = create_engine(url)
+	Session = sessionmaker(bind=engine)
+	session = Session()
 
-matches = GetFilesMatching(
-	[':00:'],
-	settings.raw_data_path(),
-	Order
-	)
-total_lines = 0
-
-url = 'postgresql://tech:tech@127.0.0.1/techwatch'
-
-engine = create_engine(url)
-Session = sessionmaker(bind=engine)
-session = Session()
-
-for idx, fname in enumerate(matches):
-	print('doing', fname)
-	result = GetAnalyzerResult(fname)
-	json_result = []
-	for jdx in range(len(result.sites)):
-		google_url = result.urls[jdx]
-		existing_or = session.query(Recommendation).filter(Recommendation.google_url==google_url).first()
-		if existing_or:
-			continue
-		r = Recommendation()
-		r.date = fname.split('date=')[-1]
-		r.google_url = google_url
-		r.publication = result.sites[jdx]
-		r.query = 'TODO'
-		r.scrape_idx = jdx
-		r.title = result.titles[jdx]
-		session.add(r)
+	for idx, fname in enumerate(matches):
+		print('doing', fname)
+		result = GetAnalyzerResult(fname)
+		json_result = []
+		for jdx in range(len(result.sites)):
+			r = Recommendation()
+			r.date = fname.split('date=')[-1]
+			r.google_url = result.urls[jdx]
+			r.publication = result.sites[jdx]
+			r.query = query_term
+			r.scrape_idx = jdx
+			r.title = result.titles[jdx]
+			# Check if this exists.
+			existing = session.query(Recommendation).filter(
+				Recommendation.date==r.date,
+				Recommendation.google_url==r.google_url,
+				Recommendation.publication==r.publication,
+				Recommendation.query==r.query,
+				Recommendation.scrape_idx==r.scrape_idx,
+				Recommendation.title==r.title,
+				).first()
+			if not existing:
+				session.add(r)
 
 session.commit()
 session.close()
--- db/size.py
+++ db/size.py
@@ -22,12 +22,12 @@ Base = declarative_base()
 class Recommendation(Base):
 	__tablename__ = 'recommendation'
 	id = Column(Integer, primary_key=True)
-	# page_views = Column(Integer)
-	# date = Column(sqlalchemy.types.Date())
-	user_id = Column(Integer)
-	publication = Column(sqlalchemy.types.String(256))
-	title = Column(sqlalchemy.types.String(1024))
+	date = Column(sqlalchemy.types.String(256))
 	google_url = Column(sqlalchemy.types.String(2048))
+	publication = Column(sqlalchemy.types.String(256))
+	query = Column(sqlalchemy.types.String(256))
+	scrape_idx = Column(Integer)
+	title = Column(sqlalchemy.types.String(1024))
 
 url = 'postgresql://tech:tech@127.0.0.1/techwatch'
 
